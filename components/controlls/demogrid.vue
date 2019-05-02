02bf6129c8e163aaabc973af8bfe2f63cb6069b6
patrick Imboden <pimboden@msn.com>
2019-05-02 22:28:33 +0200

    leccion componentes

--- components/controlls/demogrid.vue
+++ components/controlls/demogrid.vue
@@ -1,82 +1,141 @@
 <template>
-  <table>
+<table class="demogrid">
     <thead>
-      <tr>
-        <th v-for="column in columns"
-          @click="sortBy(column)"
-          :key="column"
-          :class="{ active: sortKey == column }">
-          {{ column | capitalize }}
-          <span class="arrow" :class="sortOrders[column] > 0 ? 'asc' : 'dsc'">
+        <tr>
+            <th v-for="column in columns" @click="sortBy(column)" :key="column" :class="{ active: sortKey == column }">
+                {{ column | capitalize }}
+                <span class="arrow" :class="sortOrders[column] > 0 ? 'asc' : 'dsc'">
           </span>
-        </th>
-      </tr>
+            </th>
+        </tr>
     </thead>
     <tbody>
-      <tr v-for="entry in filteredPaises" :key="entry">
-        <td v-for="key in columns"  :key="key">
-          {{entry[key]}}
-        </td>
-      </tr>
+        <tr v-for="entry in filteredDataRows" :key="entry">
+            <td v-for="key in columns" :key="key" @click="toggleEdit(key)">
+                <span v-if="!isEditMode">{{entry[key]}}</span>
+                <input v-else type="text" :value="entry[key]" />
+            </td>
+        </tr>
     </tbody>
-  </table>
+</table>
 </template>
 
-<script >
-export default{
-  props:{
-  paises:Array,
-  columns:Array,
-  filterKey:String,  
-},
-data(){
-  var sortOrders={};
-  this.columns.forEach(function(key) {
-   sortOrders[key] =1
-  })
-  return {
-    sortKey:'',
-    sortOrders:sortOrders
-  }
-
-},
-computed:{
-  filteredPaises(){    
-    var sortKey=this.sortKey
-    var filterKey=this.filterKey && this.filterKey.toLowerCase()
-    var order=this.sortOrders[sortKey]||1
-    var paises=this.paises
-    if(filterKey)
-    {
-      paises=paises.filter(function(row){
-        return Object.keys(row).some(function(key){
-          return String(row[key]).toLowerCase().indexOf(filterKey)>-1
+<script>
+export default {
+    props: {
+        dataRows: Array,
+        columns: Array,
+        filterKey: String,
+    },
+    data() {
+        var sortOrders = {};
+        this.columns.forEach(function (key) {
+            sortOrders[key] = 1
         })
-      })
-    }
-    if(sortKey){
-      paises=paises.slice().sort(function(a,b){
-        a=a[sortKey]
-        b=b[sortKey]
-        return (a===b?0:a>b?1:-1)*order
-      })
-    }
-    return paises
-  }
-},
-filters:{
-  capitalize:function(str)
-  {
-    return str.charAt(0).toUpperCase()+str.slice(1)
-  }
-},
-methods:{
-    
-      sortBy: function (key) {
-      this.sortKey = key
-      this.sortOrders[key] = this.sortOrders[key] * -1
-    }
-  }
-}
+        return {
+            sortKey: '',
+            sortOrders: sortOrders,
+            isEditMode:false
+        }
 
+    },
+    computed: {
+        filteredDataRows() {
+            var sortKey = this.sortKey
+            var filterKey = this.filterKey && this.filterKey.toLowerCase()
+            var order = this.sortOrders[sortKey] || 1
+            var dataRows = this.dataRows
+            if (filterKey) {
+                dataRows = dataRows.filter(function (row) {
+                    return Object.keys(row).some(function (key) {
+                        return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+                    })
+                })
+            }
+            if (sortKey) {
+                dataRows = dataRows.slice().sort(function (a, b) {
+                    a = a[sortKey]
+                    b = b[sortKey]
+                    return (a === b ? 0 : a > b ? 1 : -1) * order
+                })
+            }
+            return dataRows
+        }
+    },
+    filters: {
+        capitalize: function (str) {
+            return str.charAt(0).toUpperCase() + str.slice(1)
+        }
+    },
+    methods: {
+      toggleEdit(key){
+        this.isEditMode = !this.isEditMode;
+      },
+        sortBy(key) {
+          this.$emit('onEventoComponente')
+            this.sortKey = key
+            this.sortOrders[key] = this.sortOrders[key] * -1
+        }
+    }
+}
 </script>
+
+<style lang="scss">
+.demogrid {
+
+    table {
+        border: 2px solid #42b983;
+        border-radius: 3px;
+        background-color: #fff;
+    }
+
+    th {
+        background-color: #42b983;
+        color: rgba(255, 255, 255, 0.66);
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    td {
+        background-color: #f9f9f9;
+    }
+
+    th,
+    td {
+        min-width: 120px;
+        padding: 10px 20px;
+    }
+
+    th.active {
+        color: #fff;
+    }
+
+    th.active .arrow {
+        opacity: 1;
+    }
+
+    .arrow {
+        display: inline-block;
+        vertical-align: middle;
+        width: 0;
+        height: 0;
+        margin-left: 5px;
+        opacity: 0.66;
+    }
+
+    .arrow.asc {
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+        border-bottom: 4px solid #fff;
+    }
+
+    .arrow.dsc {
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+        border-top: 4px solid #fff;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,77 @@
+<template>
+<section>
 
+    <div id="demo">
+        <demo-grid :dataRows="paises" :columns="columns" :filter-key="searchQuery"  @onEventoComponente="ejecuteEvento()"/>
+    </div>
+    <div>
 
-<template>  
-<section>  
-  
-    <div id="demo">    
-    <DemoGrid
-    :paises="gridData"
-    :columns="gridColumns"
-    :filter-key="searchQuery">
-    </DemoGrid>
-</div>
-   <div>
-      
-      <h1 class="title">
-        pruebavuejs
-      </h1>
-        {{valor}}
-        <input type="text" v-model="valor" />   
+        <h1 class="title">
+            pruebavuejs
+        </h1>
+
         <button @click="getPaises" >Traiga ya carajo!</button>
 
         <div v-for="pais in paises" :key="pais.id">{{pais.id}} {{pais.nombrePais}} {{pais.codigoIso}}</div>
         <div>{{cargando}}</div>
-    </div> 
- </section>
+        <div v-if="seEjecutoEvento">se ejecuta bubu</div>
+    </div>
+</section>
 </template>
-
 
 <script>
 import DemoGrid from '~/components/controlls/demogrid'
-export default{
-  components:{
-    DemoGrid
-  }, 
+export default {
+    components: {
+        DemoGrid
+    },
+    data() {
+        return {
+            searchQuery: '',
+            columns: ['id', 'nombrePais', 'codigoIso'],
+            paises: [],
+            cargando:false,
+            seEjecutoEvento:false
+        }
+    },
+    methods: {
+      ejecuteEvento(){
+        this.seEjecutoEvento = true;
+      },
+        getPaises() {
+            debugger;
+            this.cargando = true;
+            this.paises = [{
+                    id: 1,
+                    nombrePais: "colombia",
+                    codigoIso: "col"
+                },
+                {
+                    id: 2,
+                    nombrePais: "Alemania",
+                    codigoIso: "de"
+                },
+                {
+                    id: 3,
+                    nombrePais: "Suiza",
+                    codigoIso: "ch"
+                }
+            ]
+            // this.$axios.$get('Paises/GetAll')
+            //     .then(dataDelApi=>{
+            //       debugger;
 
-  data:{
-    searchQuery:'',
-    gridColumns:['Id','nombreaPis'DOMPointReadOnly
-    gridData:[]
-  },
-  methods:{
-    getPaises(){
-      debugger;
-      this.cargando=true;
-      this.$axios.$get('Paises/GetAll')
-          .then(dataDelApi=>{
-            this.paises =JSON.parse(JSON.stringify(dataDelApi));
-          })
-          .catch(elError=>{
-            console.log(elError)
-          })
-          .finally(()=> this.cargando = false);
-        },
-  }
+            //       this.paises =JSON.parse(JSON.stringify(dataDelApi));
+            //     })
+            //     .catch(elError=>{
+            //       console.log(elError)
+            //     })
+            //     .finally(()=> this.cargando = false);
+            //   },
+        }
+    }
 }
 </script>
-
 
 <style>
 
